14df20a6e1d0ab958303c32d7ef665c61b6762de
Laumania <mads@laumania.net>
2016-05-14 20:44:04 +0200

    Added custom error page for unhandled exceptions

--- Dinero.Tools.Difference/Dinero.Tools.Difference.Web/Global.asax.cs
+++ Dinero.Tools.Difference/Dinero.Tools.Difference.Web/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
+using Dinero.Tools.Difference.Web.Controllers;
 
 namespace Dinero.Tools.Difference.Web
 {
@@ -10,6 +13,31 @@ namespace Dinero.Tools.Difference.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        public void Application_Error(Object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "ErrorPage");
+            routeData.Values.Add("action", "Error");
+            routeData.Values.Add("exception", exception);
+
+            if (exception.GetType() == typeof(HttpException))
+            {
+                routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
+            }
+            else
+            {
+                routeData.Values.Add("statusCode", 500);
+            }
+
+            Response.TrySkipIisCustomErrors = true;
+            IController controller = new ErrorPageController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+            Response.End();
         }
     }
 }
