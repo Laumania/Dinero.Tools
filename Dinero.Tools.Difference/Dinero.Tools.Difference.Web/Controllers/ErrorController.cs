e666c9c1e3063edb70117d5c7c957b151a6b0458
Laumania <mads@laumania.net>
2016-05-23 21:48:50 +0200

    Simpler error handling, lets test on Azure

---
+++ Dinero.Tools.Difference/Dinero.Tools.Difference.Web/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Dinero.Tools.Difference.Web.ViewModels;
+
+namespace Dinero.Tools.Difference.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+    }
+}
